dca408d5e023030058134db979ee453c87a3051a
Ram Jonchhen <46254432+Ramjonchhen@users.noreply.github.com>
2020-03-03 22:06:13 +0545

    Wrong Bubble sort commit coreected commit

--- main.cpp
+++ main.cpp
@@ -1,73 +1,121 @@
 #include <iostream>
 #include <graphics.h>
-#include <cstdlib>
-#include <vector>
 #include <stdio.h>
-#include <stdlib.h>
+
 using namespace std;
 
 // global variables
-int no_of_elements,arr[10],color_value[10];
-void take_input()
-{
-    int i;
-    cout<<"\n Enter the no of elements to sort: ";
-    cin>>no_of_elements;
-    cout<<"Enter the elements"<<endl;
-    for(i=0; i<no_of_elements; i++)
-    {
-        cin>>arr[i];
-    }
-}
+int values[9];
+int no_of_elements;
+    int xBlockStarting[9];
+    int yBlockStarting[9];
+    int xBlockEnd[9];
+    int yBlockEnd[9];
+    int yBase=300;
 
-void display_array()
-{
-    int i;
-    cout<<"\n The array elements are"<<endl;
-    for (i=0; i<no_of_elements; i++)
-    {
-        cout<<arr[i];
-    }
-}
+    int color[9]={0,1,2,3,4,5,6,7,8};
 
-void display_blocks()
+
+    char value[2];
+
+void display_blocks(int values[])
 {
     cleardevice();
-    char value[2];
- // drawing the blocks
-    int xStarting=300,yStarting=400,xEnd=330,yEnd=430,spacing=10;
-    for(int i=0; i<no_of_elements; i++)
+    xBlockStarting[0]=70;
+
+     for(int i=0; i<no_of_elements; i++)
     {
-            // first the y starting point is minimized to draw the blocks
-            // in our case 1-block has height of 30
-            yStarting=yEnd-(arr[i]*25);
-            // for the end point this is for width of our box
-            // we have choosen to take the width of the box as 50
-            xEnd=xStarting+40;
-            // drawing the rectangle
-            rectangle(xStarting,yStarting,xEnd,yEnd);
-            // filling the color of the boxes
-            color_value[i]=rand();
-            setfillstyle(1,color_value[i]);
-            floodfill(xStarting+10,yEnd-10,15);
+        yBlockEnd[i]=yBase;
+            int j=i;
+        yBlockStarting[i]=yBase-(20*values[i]);
+        xBlockEnd[i]=xBlockStarting[i]+40;
+        rectangle(xBlockStarting[i],yBlockStarting[i],xBlockEnd[i],yBlockEnd[i]);
+        setfillstyle(1,color[i]);
+            floodfill(xBlockStarting[i]+10,yBlockEnd[i]-10,15);
             // for spacing between the blocks we have choosen
             // the spacing betn the blocs as 10
-            sprintf(value,"%d",arr[i]);
-            outtextxy(xStarting+15,yEnd-20,value);
-            xStarting=xEnd+spacing;
+            sprintf(value,"%d",values[i]);
+            outtextxy(xBlockStarting[i]+15,yBlockEnd[i]-20,value);
+        xBlockStarting[j+1]=xBlockEnd[i]+10;
     }
+
 }
 
+
+
+void swap_blocks(int x,int y)
+{
+    int move_block[9];
+    cleardevice();
+    for (int inc=0; inc<9; inc++)
+    {
+        int temp=inc;
+        if (temp==x)
+        {
+            move_block[inc]=1;
+        }
+
+        else if (temp==y)
+        {
+            move_block[inc]=-1;
+        }
+
+        else{
+            move_block[inc]=0;
+        }
+    }
+  for(int k=0; k<=15; k++)
+  {
+      cleardevice();
+      for(int i=0; i<no_of_elements; i++)
+    {
+        yBlockEnd[i]=yBase-move_block[i]*3*k;
+            int j=i;
+        yBlockStarting[i]=yBlockEnd[i]-(20*values[i]);
+        xBlockEnd[i]=xBlockStarting[i]+40;
+        rectangle(xBlockStarting[i],yBlockStarting[i],xBlockEnd[i],yBlockEnd[i]);
+        setfillstyle(1,color[i]);
+            floodfill(xBlockStarting[i]+10,yBlockEnd[i]-10,15);
+            // for spacing between the blocks we have choosen
+            // the spacing betn the blocs as 10
+            sprintf(value,"%d",values[i]);
+            outtextxy(xBlockStarting[i]+15,yBlockEnd[i]-20-move_block[i]*i,value);
+        xBlockStarting[j+1]=xBlockEnd[i]+10;
+    }
+    delay(150);
+  }
+}
 
 
 int main()
 {
-    initwindow(1380,1200,"visualizing algorithm");
-    // taking the input for the array
-    take_input();
-    display_blocks();
+    initwindow(1000,1000,"graphics");
+    cout<<"Enter the no of elements in the array: ";
+    cin>>no_of_elements;
+    for(int i=0; i<no_of_elements; i++)
+    {
+        cin>>values[i];
+    }
+    display_blocks(values);
 
-    getch();
-
+    delay(200);
+    // performing the bubble sort
+    int temp;
+    for(int i=0; i<no_of_elements; i++)
+    {
+        for(int j=0; j<no_of_elements-1-i; j++)
+        {
+           if(values[j]>values[j+1])
+           {
+               int calltemp=j;
+               swap_blocks(calltemp,calltemp+1);
+               temp=values[j];
+               values[j]=values[j+1];
+               values[j+1]=temp;
+               display_blocks(values);
+           }
+        }
+    }
+getch();
     return 0;
 }
